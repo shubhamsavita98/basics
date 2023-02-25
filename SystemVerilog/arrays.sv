57b32008a36dc36ac94fca967793ebbc30cd449f
Shubham Kumar Savita <shubhamsavita89@gmail.com>
2023-02-25 12:39:21 -0800

    Modified for bit and logic data types

--- SystemVerilog/arrays.sv
+++ SystemVerilog/arrays.sv
@@ -1,11 +1,15 @@
 // Arrays
 module test();
   
-  int lo_hi[0:15] = '{1,2,3,4,5,6,7,6,4,6,7,5,4,5,6,7};
+  //int lo_hi[0:15] = '{1,2,3,4,5,6,7,6,4,6,7,5,4,5,6,7};
+  //bit [7:0] my_bitmem [128];
+  logic [3:0] my_logicmem [64];
   int i;
   initial begin
-    for(i=0; i<=15; i++) begin
-      $display("%d", lo_hi[i]);
+    for(i=0; i<=20; i++) begin
+      //$display("%d", lo_hi[i]); // prints 0's as 2-state data object/type
+      //$display("%d", my_bitmem[i]); // prints 0's as 2-state data object/type
+      $display("%d", my_logicmem[i]); // prints x's as 4-state data object/type
     end
   end
   
