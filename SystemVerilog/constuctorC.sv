d018994dcca27ecb9122c86aa2befc624aa5de1b
Shubham Kumar Savita <shubhamsavita89@gmail.com>
2023-05-23 15:03:07 -0700

    modified and added stdout logs

--- SystemVerilog/constuctorC.sv
+++ SystemVerilog/constuctorC.sv
@@ -1,5 +1,7 @@
 // Code your testbench here
 // or browse Examples
+// Code your testbench here
+// or browse Examples
 class Transaction;
   logic [31:0] addr, csm, data[8];
   
@@ -22,9 +24,11 @@ class Driver;
 endclass 
 
 module tb;
+
   
 initial begin
   Transaction tr;
+  Driver dr;
   tr = new(.a(10));
   $display("Value of addr: %0d", tr.addr);
   $display("Value of data: %0p", tr.data);
@@ -34,17 +38,9 @@ initial begin
   tr = new(.a(2),.b(8));
   $display("Value of addr: %0d", tr.addr);
   $display("Value of data: %0p", tr.data);
-
+  dr = new();
 end
   
-/*you cannot have this driver class object creation
-  in one single initial block, you need to separate
-  out in other initial block as below.
-*/
-initial begin
-    Driver dr;
-    dr = new();
-end
 
 initial begin
   $dumpfile("file.vcd");
@@ -53,17 +49,16 @@ initial begin
 end
 endmodule 
 
-
 ///////////////////////////////OUTPUT
 /*
-[2023-05-23 17:21:49 EDT] vcs -licqueue '-timescale=1ns/1ns' '+vcs+flush+all' '+warn=all' '-sverilog' design.sv testbench.sv  && ./simv +vcs+lic+wait  
+[2023-05-23 18:01:06 EDT] vcs -licqueue '-timescale=1ns/1ns' '+vcs+flush+all' '+warn=all' '-sverilog' design.sv testbench.sv  && ./simv +vcs+lic+wait  
 
 Warning-[LINX_KRNL] Unsupported Linux kernel
   Linux kernel '3.13.0-71-generic' is not supported.
   Supported versions are 2.4* or 2.6*.
 
                          Chronologic VCS (TM)
-            Version S-2021.09 -- Tue May 23 17:21:50 2023
+            Version S-2021.09 -- Tue May 23 18:01:07 2023
 
                     Copyright (c) 1991 - 2021 Synopsys, Inc.
    This software and the associated documentation are proprietary to Synopsys,
@@ -94,10 +89,10 @@ rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
 if [ -x ../simv ]; then chmod a-x ../simv; fi
 g++  -o ../simv      -m32 -m32 -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/S-2021.09/linux/lib -L/apps/vcsmx/vcs/S-2021.09/linux/lib  -Wl,-rpath-link=./ -Wl,--no-as-needed   objs/amcQw_d.o   _320_archive_1.so  SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o           -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/S-2021.09/linux/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/S-2021.09/linux/lib/vcs_save_restore_new.o /apps/vcsmx/vcs/S-2021.09/linux/lib/ctype-stubs_32.a -ldl  -lc -lm -lpthread -ldl 
 ../simv up to date
-CPU time: .230 seconds to compile + .286 seconds to elab + .272 seconds to link
+CPU time: .234 seconds to compile + .320 seconds to elab + .312 seconds to link
 Chronologic VCS simulator copyright 1991-2021
 Contains Synopsys proprietary information.
-Compiler version S-2021.09; Runtime version S-2021.09;  May 23 17:21 2023
+Compiler version S-2021.09; Runtime version S-2021.09;  May 23 18:01 2023
 Value of addr: 10
 Value of data: '{'h5, 'h5, 'h5, 'h5, 'h5, 'h5, 'h5, 'h5} 
 Value of addr: 3
@@ -107,8 +102,8 @@ Value of data: '{'h8, 'h8, 'h8, 'h8, 'h8, 'h8, 'h8, 'h8}
 Transaction addr value inside driver cls: 3
            V C S   S i m u l a t i o n   R e p o r t 
 Time: 0 ns
-CPU Time:      0.560 seconds;       Data structure size:   0.0Mb
-Tue May 23 17:21:52 2023
+CPU Time:      0.480 seconds;       Data structure size:   0.0Mb
+Tue May 23 18:01:09 2023
 Done
 */
 /////////////////////////////////////////////////////////
