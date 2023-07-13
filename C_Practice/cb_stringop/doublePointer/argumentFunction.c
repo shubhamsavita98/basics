3869926a88c2d1ddb7b3bbc63367fec494971856
shubham <ssavita@sfsu.edu>
2023-07-13 12:59:15 -0700

    pointer to pointer

---
+++ C_Practice/cb_stringop/doublePointer/argumentFunction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Checking the command line argument functionality - type 1
+//int main(int argc, char** argv)
+int main(int argc, char* argv[])
+{
+    printf("Hello this is command line argument program!\n");
+    printf("Total number of arguments are %d.\n", argc);
+    for(int i=0; i<argc; i++){
+        printf("%s\n", argv[i]);
+    }
+    return 0;
+}
+
